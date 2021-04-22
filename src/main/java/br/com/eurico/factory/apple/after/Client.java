a940d6e82a32bd1262757d7d7abc11a51b554f3a
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-21 22:03:32 -0300

    criando xsmax example

--- src/main/java/br/com/eurico/factory/apple/after/Client.java
+++ src/main/java/br/com/eurico/factory/apple/after/Client.java
@@ -3,6 +3,7 @@ package br.com.eurico.factory.apple.after;
 import br.com.eurico.factory.apple.after.factory.IPhone11ProFactory;
 import br.com.eurico.factory.apple.after.factory.IPhoneFactory;
 import br.com.eurico.factory.apple.after.factory.IPhoneXFactory;
+import br.com.eurico.factory.apple.after.factory.IPhoneXSMaxFactory;
 import br.com.eurico.factory.apple.after.model.IPhone;
 
 public class Client {
@@ -10,6 +11,7 @@ public class Client {
 	public static void main(String[] args) {
 		IPhoneFactory iphoneXFactory = new IPhoneXFactory();
 		IPhoneFactory iphone11ProFactory = new IPhone11ProFactory();
+		IPhoneFactory iphoneXSMaxFactory = new IPhoneXSMaxFactory();
 		
 		System.out.println("### Ordering an iPhone X");
 		IPhone iphone = iphoneXFactory.orderIPhone();
@@ -18,5 +20,9 @@ public class Client {
 		System.out.println("\n\n### Ordering an iPhone 11 HighEnd");
 		IPhone iphone2 = iphone11ProFactory.orderIPhone();
 		System.out.println(iphone2);
+		
+		System.out.println("\n\n### Ordering an iPhone XSMax");
+		IPhone iphone3 = iphoneXSMaxFactory.orderIPhone();
+		System.out.println(iphone3);
 	}
 }
