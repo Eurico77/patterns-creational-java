43ae305dae6e2924c30991dfc8d00280857acd74
Eurico Magalhães <euricomagalhaes61@gmail.com>
2021-04-23 23:55:21 -0300

    ajustes

--- src/main/java/br/com/eurico/factory/apple/simple/factory/IPhoneSimpleFactory.java
+++ src/main/java/br/com/eurico/factory/apple/simple/factory/IPhoneSimpleFactory.java
@@ -8,28 +8,28 @@ import br.com.eurico.factory.apple.simple.model.IPhoneXSMax;
 
 public class IPhoneSimpleFactory {
 
-	public static IPhone orderIPhone(String generation, String level) {
-		IPhone device = null;
+  public static IPhone orderIPhone(String generation, String level) {
+    IPhone device = null;
 
-		if (generation.equals("X")) {
-			if (level.equals("standard")) {
-				device = new IPhoneX();
-			} else if (level.equals("highEnd")) {
-				device = new IPhoneXSMax();
-			}
-		} else if (generation.equals("11")) {
-			if (level.equals("standard")) {
-				device = new IPhone11();
-			} else if (level.equals("highEnd")) {
-				device = new IPhone11Pro();
-			}
-		}
+    if (generation.equals("X")) {
+      if (level.equals("standard")) {
+        device = new IPhoneX();
+      } else if (level.equals("highEnd")) {
+        device = new IPhoneXSMax();
+      }
+    } else if (generation.equals("11")) {
+      if (level.equals("standard")) {
+        device = new IPhone11();
+      } else if (level.equals("highEnd")) {
+        device = new IPhone11Pro();
+      }
+    }
 
-		device.getHardware();
-		device.assemble();
-		device.certificates();
-		device.pack();
+    device.getHardware();
+    device.assemble();
+    device.certificates();
+    device.pack();
 
-		return device;
-	}
+    return device;
+  }
 }
